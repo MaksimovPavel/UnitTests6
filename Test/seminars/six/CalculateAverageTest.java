88566c7203fc51958cd8ccaba037572466977bf3
MaksimovPavel <102986563+MaksimovPavel@users.noreply.github.com>
2024-03-07 09:01:08 +0300

    refactoring CalculateAverageTest.java

--- Test/seminars/six/CalculateAverageTest.java
+++ Test/seminars/six/CalculateAverageTest.java
@@ -15,15 +15,24 @@ class CalculateAverageTest {
 
         assertEquals(expectedAverage, actualAverage);
     }
-
     @Test
     void testCompareAverage() {
-
         double average1 = 5.0;
         double average2 = 6.0;
+        double average3 = 7.0;
+        double average4 = 7.0;
+        double average5 = 9.0;
+        double average6 = 8.0;
         String expectedResult = "Второй список имеет большее среднее значение";
+        String expectedResult1 = "Средние значения равны";
+        String expectedResult2 = "Первый список имеет большее среднее значение";
+        
         String actualResult = CalculateAverage.compareAverage(average1, average2);
+        String actualResult1 = CalculateAverage.compareAverage(average3, average4);
+        String actualResult2 = CalculateAverage.compareAverage(average5, average6);
 
         assertEquals(expectedResult, actualResult);
+        assertEquals(expectedResult1, actualResult1);
+        assertEquals(expectedResult2, actualResult2);
     }
 }
